30ca160384709b646038111249f7e03bb1d15e90
Otloir <oli.toreland@gmail.com>
2026-03-02 12:24:46 +0100

    added a pagination to the products/index file

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::All();
+        $products = Product::paginate(10);
         return view('products.index', ['products' => $products]);
     }
 
