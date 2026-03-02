379f2a3972765c8b54808a88db232bfb5848034e
Malinsson <malin@The-mole.localdomain>
2026-03-02 21:47:14 +0100

    Added toggle to show login form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,12 +11,12 @@
 
     <header class="absolute w-full bg-transparent h-24 text-white py-4">
         <div class="container mx-auto flex justify-between items-center">
-            <a href="/" class="text-lg font-bold">Glasolsson</a>
+            <a href="/" class="text-lg font-bold">Glas Olsson</a>
             <nav>
                 <ul class="flex space-x-4">
                     <li><a href="{{ route('categories.index') }}" class="hover:underline">Kategorier</a></li>
                     <li><a href="{{ route('products.index') }}" class="hover:underline">Produkter</a></li>
-                    <li><a href="/dashboard" class="hover:underline">Dashboard</a></li>
+                    <li><a href="#" id="login-toggle" class="hover:underline cursor-pointer">Login</a></li>
                 </ul>
             </nav>
         </div>
@@ -24,14 +24,16 @@
 
 
     <main class="min-h-screen">
+
         @include('errors')
         @yield('content')
+
     </main>
 
 
     <footer class="bg-slate-700 text-white py-4">
         <div class="container mx-auto text-center">
-            &copy; {{ date('Y') }} Glasolsson. All rights reserved.
+            &copy; {{ date('Y') }} Glas Olsson. All rights reserved.
         </div>
     </footer>
 
