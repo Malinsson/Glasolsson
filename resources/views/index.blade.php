fb2a719ecd9b9b20e6154e4e81eb9f7f05323736
Malinsson <malin@The-mole.localdomain>
2026-03-02 19:52:48 +0100

    Fixed issues with background and made temporary login form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,18 +4,30 @@
 
 @section('content')
 
+<div style="background-image: url('{{ asset('images/background.png') }}');" class="bg-cover bg-center min-h-screen">
 
-<form method="post" action="/login">
-    <div>
-        <label for="email">Email</label>
-        <input name="email" id="email" type="email" />
+    <div class="flex items-center justify-center h-screen">
+        <div class="bg-white bg-opacity-75 p-8 rounded shadow-md w-full max-w-md">
+            <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
+            <form method="post" action="/login">
+                @csrf
+                <div>
+                    <label for="email" class="block text-sm/6 font-medium text-black">Email</label>
+                    <div class="mt-2">
+                        <input name="email" id="email" type="email" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"/>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <label for="password" class="block text-sm/6 font-medium text-black">Password</label>
+                    <input name="password" id="password" type="password" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"/>
+                </div>
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+            <button type="submit" class="mt-4 w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">Login</button>
+            
+            </form>
+        </div>
     </div>
-    <div>
-        <label for="password">Password</label>
-        <input name="password" id="password" type="password" />
-    </div>
-    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-    <button type="submit">Login</button>
-</form>
+</div>
+
 
 @endsection
