3daa88d73a98d388023d53e821d8a759a3bb064d
Malinsson <malin@The-mole.localdomain>
2026-03-02 21:47:14 +0100

    Added toggle to show login form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,15 +6,16 @@
 
 <div style="background-image: url('{{ asset('images/background.png') }}');" class="bg-cover bg-center min-h-screen">
 
-    <div class="flex items-center justify-center h-screen">
+    <div id="login-form" class="hidden flex items-center justify-center h-screen">
         <div class="bg-white bg-opacity-75 p-8 rounded shadow-md w-full max-w-md">
+            <span id="close-login" tabindex="0" class="focus:outline-2 focus:outline-indigo-500 rounded cursor-pointer float-right text-black text-xl hover:text-gray-600">×</span>
             <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
             <form method="post" action="/login">
                 @csrf
                 <div>
                     <label for="email" class="block text-sm/6 font-medium text-black">Email</label>
                     <div class="mt-2">
-                        <input name="email" id="email" type="email" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"/>
+                        <input name="email" id="email" type="email" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 invalid:border-red-500"/>
                     </div>
                 </div>
                 <div class="mt-4">
@@ -22,7 +23,7 @@
                     <input name="password" id="password" type="password" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"/>
                 </div>
             <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-            <button type="submit" class="mt-4 w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">Login</button>
+            <button type="submit" class="mt-4 w-full bg-slate-600 hover:bg-slate-800 text-white font-bold py-2 px-4 rounded cursor-pointer">Login</button>
             
             </form>
         </div>
