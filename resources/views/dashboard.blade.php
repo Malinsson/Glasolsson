58df8d42e721531ff120950f7c65bfc7b233e2ef
Malinsson <malin@The-mole.localdomain>
2026-03-03 16:23:50 +0100

    Removed middleware(guest) for index page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,14 @@
-@include('errors')
+@extends('layouts.app')
+
+@section('title', 'Dashboard - Glas Olsson')
+
+@section('content')
+
+<div class="h-24 bg-slate-800"></div>
+
 <p>Hello, {{ auth()->user()->name }}</p>
 
 <hr>
 <a href="/logout">Logout</a>
+
+@endsection
