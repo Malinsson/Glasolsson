38b077a73f9fdf056a22c2f1ec51ba71ffd7e03e
Malinsson <malin@The-mole.localdomain>
2026-03-05 22:47:55 +0100

    Added styling to product.edit

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -39,6 +39,7 @@
 
         <label for="image">Produktbild</label>
         <input type="file" name="image" id="image" accept="image/jpg, image/jpeg, image/webp, image/png, image/avif" class="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500" onchange="validateImage()">
+        
         <p id="image-error" style="color: red; display: none;"></p>
 
         <button type="submit" class="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">
