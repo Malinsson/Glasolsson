0fa2ce8435170427bfa2d7f7a477f90e4128a8e8
Malinsson <malin@The-mole.localdomain>
2026-03-09 13:44:53 +0100

    Added styling to product.edit for desktop

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -4,22 +4,31 @@
 
 @section('content')
     
-<a href="{{ route('products.index') }}"><- Tillbaka</a> 
+<section class="font-text flex flex-col gap-3 m-4 mt-12 w-full md:flex-row md:gap-6">
 
-<section class="font-text flex flex-col flex-wrap gap-3 m-4">
+    <div class="w-full md:w-1/2 flex flex-col gap-3">
+        <p>Id: {{$product->id}}</p>
+        <h2 class="text-2xl">{{$product->name}}</h2> 
+        <p class="font-bold md:text-xl">Material:</p>
+        <p>{{$product->material}}</p>
+        <p class="font-bold md:text-xl">Färg:</p>
+        <p>{{$product->color}}</p>
+        <p class="font-bold md:text-xl">Pris:</p>
+        <p>{{$product->price}} kr</p>
+        <p class="font-bold md:text-xl">Kategori:</p>
+        <p>{{$product->category->name}}</p>
+    </div>
 
-    <p>Id: {{$product->id}}</p>
-    <h2 class="text-2xl">{{$product->name}}</h2> 
-    <p>Material: {{$product->material}}</p>
-    <p>Färg: {{$product->color}}</p>
-    <p>Pris: {{$product->price}} kr</p>
-    <p>Kategori: {{$product->category->name}}</p>
-    <img src="{{ str_starts_with($product->image, 'images/stock/') ? asset($product->image) : asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="w-full h-auto object-cover">
-    <p>{{$product->description}}</p>
+    <div class="w-full md:w-1/2 flex flex-col gap-3">
+        <p class="font-bold md:text-xl">Produktbild:</p>
+        <img src="{{ str_starts_with($product->image, 'images/stock/') ? asset($product->image) : asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="w-full h-auto object-cover">
+        <p class="font-bold md:text-xl">Beskrivning:</p>
+        <p>{{$product->description}}</p>
+    </div>
 
 </section>
 
-<div class="flex flex-row justify-between gap-3 m-8">
+<div class="flex flex-row justify-between gap-3 m-8 md:justify-start">
     <a href="{{ route('products.edit', $product->id) }}" class="bg-slate-600 hover:bg-slate-800 text-white font-bold py-2 px-4 rounded cursor-pointer">Redigera produkt</a>
 
     <form action="{{ route('products.destroy', ['product' => $product->id]) }}" method="POST" style="display:inline;">
