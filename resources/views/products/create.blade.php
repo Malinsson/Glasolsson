3c62a4c2afaca983b01a4b591c466286a64b37a3
Malinsson <malin@The-mole.localdomain>
2026-03-10 17:08:47 +0100

    Added styling to select img button>

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -38,11 +38,11 @@
         </select>
 
         <label for="image">Produktbild</label>
-        <input type="file" name="image" id="image" accept="image/jpg, image/jpeg, image/webp, image/png, image/avif" class="border border-gray-300 rounded-md py-2 px-4 cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500" onchange="validateImage()">
+        <input type="file" name="image" id="image" accept="image/jpg, image/jpeg, image/webp, image/png, image/avif" class="text-white bg-slate-500 border border-gray-300 hover:bg-slate-800 rounded-md py-2 px-4 cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500" onchange="validateImage()">
 
         <p id="image-error" style="color: red; display: none;"></p>
 
-        <button type="submit" class="bg-slate-500 hover:bg-slate-700 text-white py-2 px-4 rounded">
+        <button type="submit" class="bg-slate-700 hover:bg-slate-800 text-white py-2 px-4 cursor-pointer rounded">
             <strong>Skapa produkt</strong>
         </button>
     </form>
