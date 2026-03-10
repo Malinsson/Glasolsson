9d1f34c7b1980840f33669958c9e2f84bb46e569
Malinsson <malin@The-mole.localdomain>
2026-03-10 16:31:25 +0100

    Added max height of image for product cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,18 +31,20 @@
     </div>
 </div>
 
-<section id="products" class="flex flex-wrap justify-center pt-8 pb-16 bg-gray-200">
+<section id="products" class="flex flex-wrap justify-center pt-8 pb-16 bg-gray-200 ">
     <h2 class="text-4xl font-logo mb-8 text-center w-full"><strong>Produkter</strong></h2>
-
-    @foreach ($products as $product)
-        <div tabindex="0" class="bg-white w-sm flex flex-col justify-between items-center py-10 px-4 shadow-md m-4 hover:shadow-lg transition-shadow duration-300 rounded">
-            <img src="{{ str_starts_with($product->image, 'images/stock/') ? asset($product->image) : asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="w-full h-auto object-cover">
-            <h3 class="text-text mt-2"><strong>{{ $product->name }}</strong></h3>
-            <p class="mt-2"><span>{{ $product->color }}</span> <span>{{ $product->category?->name ?? 'Ingen kategori' }}</span></p>
-            <p class="text-lg mt-2"><strong>{{ number_format($product->price, 2) }}:-</strong></p>
-        </div>
-    @endforeach
-
+    <div class="flex flex-wrap justify-center pt-4 max-w-7xl mx-auto" >
+        @foreach ($products as $product)
+            <div tabindex="0" class="bg-white w-sm flex flex-col justify-between items-center py-10 px-4 shadow-md m-4 hover:shadow-lg transition-shadow duration-300 rounded">
+                <div class="w-full h-64 flex items-center justify-center rounded overflow-hidden">
+                    <img src="{{ str_starts_with($product->image, 'images/stock/') ? asset($product->image) : asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="w-full h-auto object-cover">
+                </div>
+                <h3 class="text-text mt-2"><strong>{{ $product->name }}</strong></h3>
+                <p class="mt-2"><span>{{ $product->color }}</span> <span>{{ $product->category?->name ?? 'Ingen kategori' }}</span></p>
+                <p class="text-lg mt-2"><strong>{{ number_format($product->price, 2) }}:-</strong></p>
+            </div>
+        @endforeach
+    </div>
     <div class="w-full flex justify-center mt-8">
         {{ $products->links() }}
     </div>
