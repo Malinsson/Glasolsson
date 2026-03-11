8941583a0f0cff0d74688cf4df4a1437a2310001
Otloir <oli.toreland@gmail.com>
2026-03-11 15:25:40 +0100

    added some styling and media quesries to product and category index

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,29 +4,29 @@
 
 @section('content')
 
-<section class="pb-3 pt-3">
+<section class="size-full pb-3 pt-3">
 
-    <div class="flex items-center justify-between">
-        <h2 class="p-3 text-2xl">Kategorier</h2>
+    <div class="flex items-center justify-between pt-6 p-3 pb-13">
+        <h2 class="text-2xl">Kategorier</h2>
         <a class="bg-slate-600 hover:bg-slate-800 text-white font-bold py-2 px-4 rounded cursor-pointer" href="{{ route('categories.create') }}">Skapa ny kategori</a>
     </div>
 
     <div class="border border-gray-400 flex-1 bg-white p-4">
 
-        <table class="w-full border-collapse text-sm text-left" aria-label="navigation list for extended information about products">
+        <table class="table" aria-label="navigation list for extended information about products">
     
             <thead>
                 <tr>
-                    <th class="table-col">Namn</th>
-                    <th class="table-col"></th>
+                    <th class="th-col">Namn</th>
+                    <th class="th-col"></th>
                 </tr>
             </thead>
     
             <tbody>
                 @foreach ($categories as $category)
-                <tr class="table-row"  aria-label="navigation for information about the {{ $category->name }}"> 
-                    <td class="table-cell">{{ $category->name }}</td>
-                    <td class="table-cell" onclick="window.location='{{ route('categories.show', $category->id)}}'">
+                <tr class="tr-row"  aria-label="navigation for information about the {{ $category->name }}"> 
+                    <td class="td-cell">{{ $category->name }}</td>
+                    <td class="td-cell" onclick="window.location='{{ route('categories.show', $category->id)}}'">
                         <div class="flex items-center place-content-center gap-2">
                             <img src="{{ asset('icons/edit.svg')}}" alt="pen" class="w-6 h-6">
                             <span>Ändra</span>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 
-<section class="pb-3 pt-3">
+<section class="size-full pb-3 pt-3">
+    {{-- 285 --}}
     
-    
-    <section class="pb-1">
+    <section class="p-3 pb-1">
         
         <div class="flex items-center justify-between pt-3 pb-12">
             <h2 class="text-2xl">Produkter</h2>
@@ -21,7 +21,7 @@
 
             <p class="text-gray-500">{{ $products->total() }} produkter hittades</p>
     
-            <div class="flex flex-row justify-between items-end">
+            <div class="hidden sm:flex flex-row justify-between items-end">
 
                 {{-- Categories --}}
                 <div class="flex flex-col gap-1">
@@ -40,7 +40,7 @@
                 </div>
             
                 {{-- Materials --}}
-                <div class="flex flex-col gap-1">
+                <div class="hidden lg:flex flex-col">
                     <label class="text-sm font-bold text-gray-700">Material</label>
                     <select name="materials[]" class="text-gray-500 border border-gray-400 bg-white font-bold py-2 px-4 rounded cursor-pointer">
                         <option value="" disabled selected hidden>Välj material</option>
@@ -56,7 +56,7 @@
                 </div>
             
                 {{-- Colors --}}
-                <div class="flex flex-col gap-1">
+                <div class="hidden lg:flex flex-col">
                     <label class="text-sm font-bold text-gray-700">Färg</label>
                     <select name="colors[]" class="text-gray-500 border border-gray-400 bg-white font-bold py-2 px-4 rounded cursor-pointer">
                         <option value="" disabled selected hidden>Välj färg</option>
@@ -72,7 +72,7 @@
                 </div>
             
                 {{-- Price --}}
-                <div class="flex flex-col gap-1">
+                <div class="hidden md:flex flex-col gap-1">
                     <label class="text-sm font-bold text-gray-700">
                         Max Pris: <span id="priceDisplay">{{ request('max_price', 3500) }} kr</span>
                     </label>
@@ -89,11 +89,11 @@
                 </div>
             
                 {{-- Buttons --}}
-                <div class="flex gap-2 items-center">
+                <div class="flex flex-wrap-reverse items-center">
                     <button type="submit" class="bg-slate-600 hover:bg-slate-800 text-white font-bold py-2 px-4 rounded">
                         Applicera
                     </button>
-                    <a href="{{ route('products.index') }}" class="text-gray-500 hover:underline px-2">
+                    <a href="{{ route('products.index') }}" class="text-gray-500 hover:underline py-3 px-4 text-">
                         Återställ
                     </a>
                 </div>
@@ -102,36 +102,33 @@
             
         </form>
     </section>
-    
-    <section class="border border-gray-400 flex-1 bg-white p-4">
+    <section class="border border-gray-400 flex-1 bg-white p-4 ">
         
         {{-- Products table --}}
-        <table class="w-full border-collapse text-sm text-left " aria-label="navigation list for extended information about products">
+        <table class="table" aria-label="navigation list for extended information about products">
             
             <thead>
                 <tr>
-                    <th class="table-col">ID</th>
-                    <th class="table-col">Namn</th>
-                    <th class="table-col">Material</th>
-                    <th class="table-col">Färg</th>
-                    <th class="table-col">Pris</th>
-                    <th class="table-col">Kategori</th>
-                    <th class="table-col"></th>
+                    <th class="th-col">ID</th>                              
+                    <th class="th-col">Namn</th>                         
+                    <th class="th-col hidden sm:table-cell">Kategori</th>   
+                    <th class="th-col hidden md:table-cell">Pris</th>    
+                    <th class="th-col hidden lg:table-cell">Material</th>
+                    <th class="th-col hidden lg:table-cell">Färg</th>    
+                    <th class="th-col"></th>                           
                 </tr>
             </thead>
-    
+            
             <tbody>
                 @forelse ($products as $product)
-                    <tr class="table-row" aria-label="a table list of the products"> 
-                        <td class="table-cell"> {{ $product->id }} </td>
-                        <td class="table-cell"> {{ $product->name }} </td> 
-                        <td class="table-cell"> {{ $product->material }} </td>  
-                        <td class="table-cell"> {{ $product->color }} </td> 
-                        <td class="table-cell"> {{ $product->price }} kr</td>  
-                        <td class="table-cell">
-                            {{ $product->category->name ?? 'Ingen kategori' }}
-                        </td>
-                        <td class="table-cell cursor-pointer" onclick="window.location='{{ route('products.show', $product->id) }}'">
+                    <tr class="tr-row">
+                        <td class="td-cell">{{ $product->id }}</td>
+                        <td class="td-cell">{{ $product->name }}</td>
+                        <td class="td-cell hidden sm:table-cell">{{ $product->category->name ?? 'Ingen kategori' }}</td>
+                        <td class="td-cell hidden md:table-cell">{{ $product->price }} kr</td>
+                        <td class="td-cell hidden lg:table-cell">{{ $product->material }}</td>
+                        <td class="td-cell hidden lg:table-cell">{{ $product->color }}</td>
+                        <td class="td-cell cursor-pointer" onclick="window.location='{{ route('products.show', $product->id) }}'">
                             <div class="flex items-center place-content-center gap-2">
                                 <img src="{{ asset('icons/edit.svg')}}" alt="pen" class="w-6 h-6">
                                 <span>Ändra</span>
